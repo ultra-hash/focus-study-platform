12ecde59aae821243a30b2ccbd567d68218c4a25
ultra-hash <jarviscommand3@gmail.com>
2023-02-13 13:05:22 +0000

    (added) - configured statc files folder

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class path(models.Model):
+    path_id = models.UUIDField(uuid.uuid4, primary_key=True)
+    path_author = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now=True)
--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from FSP.settings import STATICFILES_DIRS
+import json
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World")
+    with open(f'{STATICFILES_DIRS[0]}/solution.json', 'r') as file:
+        json_data = json.load(file)
+    return render(request, 'path.html', {"list_of_dict": json_data})
