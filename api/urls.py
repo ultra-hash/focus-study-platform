7accb9f4a25633c82c67a50e451d6e9e45a1ab61
ultra-hash <jarviscommand3@gmail.com>
2023-02-13 18:13:43 +0000

    (added and updated) - path view and update home view along with templates

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name="api.home"),
+    path('path/<str:path_name>/', views.path, name="api.path"),
 ]
--- api/views.py
+++ api/views.py
@@ -5,6 +5,12 @@ import json
 # Create your views here.
 
 def home(request):
-    with open(f'{STATICFILES_DIRS[0]}/solution.json', 'r') as file:
-        json_data = json.load(file)
-    return render(request, 'path.html', {"list_of_dict": json_data})
+    return render(request, 'home.html', {})
+
+def path(reqest, path_name):
+    if path_name == "python" or path_name == "git":
+        with open(f'{STATICFILES_DIRS[0]}/{path_name}.json', 'r') as file:
+            json_data = json.load(file)
+        return render(reqest, "path.html", {"list_of_dict": json_data})
+    else:
+        return HttpResponse("<h1>404 Page Not Found</h1>")
